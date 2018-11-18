7e8bd8e6cd3c3265831c7a66383881b821b311d2
Admin <Admin@REASON>
2018-11-18 17:30:19 +0545

    Upload All ASP.net MVC default Templete

---
+++ Bio-Book-App/Bio-Book/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bio_Book.Startup))]
+namespace Bio_Book
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
